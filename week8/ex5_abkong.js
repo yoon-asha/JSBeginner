617ccbf6318963daa1d47e7f90a86206752eea66
hhp227study <83169005+hhp227study@users.noreply.github.com>
2023-07-20 22:31:25 +0900

    abkong - eighth week solution updated

---
+++ week8/ex5_abkong.js
@@ -0,0 +1,17 @@
+/**
+모의고사
+https://school.programmers.co.kr/learn/courses/30/lessons/42840
+*/
+
+function solution(answers) {
+    var answer = [];
+    var soopojaSubmits = [[1, 2, 3, 4, 5], [2, 1, 2, 3, 2, 4, 2, 5], [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]];
+    var soopojaAnswers = [0, 0, 0];
+    
+    for (let i in answers) 
+        for (let j in soopojaSubmits) 
+            if (answers[i] == soopojaSubmits[j][i]) soopojaAnswers[j]++;
+    for (let k in soopojaAnswers) 
+        if (soopojaAnswers[k] == Math.max(...soopojaAnswers)) answer.push(parseInt(k) + 1);
+    return answer;
+}
