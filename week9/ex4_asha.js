2768f61435bed68516bd0f485846e53fa1b1de2a
yoon-asha <yoon.asha97@gmail.com>
2023-08-01 11:55:23 +0900

    asha - week9 solution updated

--- week9/ex4_asha.js
+++ week9/ex4_asha.js
@@ -1,12 +1,12 @@
 function solution(skill, skill_trees) {
   var answer = 0
+  let s = skill.split('')
 
   for (let i = 0; i < skill_trees.length; i++) {
-    let s = skill.split('')
     let tree = skill_trees[i].split('').filter((el) => s.includes(el))
     let check = true
-    for (let j = 0; j < skill.length; j++) {
-      if (tree[j] && skill[j] !== tree[j]) check = false
+    for (let j = 0; j < tree.length; j++) {
+      if (skill[j] !== tree[j]) check = false
     }
     if (check) answer++
   }
--- week9/ex5_asha.js
+++ week9/ex5_asha.js
@@ -3,11 +3,9 @@ function solution(people, limit) {
   people.sort((a, b) => b - a)
   for (let x of people) {
     if (x + people.at(-1) <= limit) {
-      answer++
       people.pop()
-    } else {
-      answer++
     }
+    answer++
   }
   return answer
 }
