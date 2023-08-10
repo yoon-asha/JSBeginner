a0383bce61d2d84b16e6b6f64ea79c439cb654b4
yoon-asha <yoon.asha97@gmail.com>
2023-08-10 23:38:08 +0900

    asha - week11 solution updated

--- week11/ex4_asha.js
+++ week11/ex4_asha.js
@@ -1,3 +1,4 @@
+/*
 function solution(clothes) {
   var answer = 1
   const hash = {}
@@ -15,3 +16,34 @@ function solution(clothes) {
 
   return answer - 1
 }
+*/
+
+// 역시 Map이 더 빠르다,,,
+function solution(clothes) {
+  var answer = 1
+  const hash = new Map()
+
+  for (let i = 0; i < clothes.length; i++) {
+    hash.set(clothes[i][1], hash.get(clothes[i][1]) + 1 || 1)
+  }
+
+  hash.forEach((el) => {
+    answer *= el + 1
+  })
+  return answer - 1
+}
+
+console.log(
+  solution([
+    ['yellow_hat', 'headgear'],
+    ['blue_sunglasses', 'eyewear'],
+    ['green_turban', 'headgear'],
+  ])
+)
+console.log(
+  solution([
+    ['crow_mask', 'face'],
+    ['blue_sunglasses', 'face'],
+    ['smoky_makeup', 'face'],
+  ])
+)
